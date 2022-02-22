07c879d4eabf1f89ee4fa049d450dc1c7b1c8fc8
hjm8333 <hjm8333@naver.com>
2022-02-22 06:42:09 +0000

    TodoList TodoItem onToggle onRemove event test ok

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import TodoItem from './Todoitem';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onToggle, onRemove }) => {
     return (
         <ul>
             {todos.map((todo) => (
-                <TodoItem todo={todo} key={todo.id} />
+                <TodoItem
+                    todo={todo}
+                    key={todo.id}
+                    onToggle={onToggle}
+                    onRemove={onRemove}
+                />
             ))}
         </ul>
     );
